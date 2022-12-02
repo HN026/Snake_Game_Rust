a8332763e662f0171ede09371ee3b45bcb3c8273
Huzaifa Naseer <101128679+HN026@users.noreply.github.com>
2022-12-02 14:46:48 +0530

    Update draw.rs

--- src/draw.rs
+++ src/draw.rs
@@ -8,6 +8,7 @@ pub fn to_coord(game_coord: i32) -> f64 {
     (game_coord as f64) * BLOCK_SIZE
 }
 
+
 pub fn draw_block(color: Color, x: i32, y: i32, con:&Context, g: &mut G2d) {
     let gui_x = to_coord(x);
     let gui_y = to_coord(y);
